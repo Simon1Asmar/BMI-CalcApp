2f8a217259a299eedbc8949ced48bed123f402c3
Simon1GA <72914663+Simon1GA@users.noreply.github.com>
2021-03-15 13:56:15 +0200

    Version 1.0

---
+++ app/src/main/java/com/simongeorgeasmar/bmi_calcapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.simongeorgeasmar.bmi_calcapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtWeight;
+    EditText edtHeight;
+    TextView txtViewAns;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        setupOnCreate();
+    }
+
+    private void setupOnCreate() {
+        edtWeight = findViewById(R.id.edtWeight);
+        edtHeight = findViewById(R.id.edtHeight);
+        txtViewAns = findViewById(R.id.txtViewAns);
+    }
+
+
+    public void btn_onClick(View view) {
+        double weight = Double.parseDouble(edtWeight.getText().toString());
+        double height = Double.parseDouble(edtHeight.getText().toString());
+
+        double bmi = weight/Math.pow(height,2);
+
+        String category = "";
+
+        if(bmi<15){
+            category="Very severely underweight";
+        } else if(bmi>=15 && bmi<=16) {
+            category = "Severely underweight";
+        } else if(bmi>16 && bmi<=18.5){
+            category = "Underweight";
+        } else if(bmi>18.5 && bmi<=25){
+            category = "Normal";
+        } else if(bmi>25 && bmi<=30){
+            category = "Overweight";
+        } else if(bmi>30 && bmi<=35){
+            category = "Moderately obese";
+        } else if(bmi>35 && bmi<=40){
+            category = "Severely obese";
+        } else if(bmi>40){
+            category = "Very severely obese";
+        }
+
+        String output = "BMI = " + bmi +"\nCategory: " +category;
+
+        txtViewAns.setText(output);
+
+        Toast.makeText(this, "BMI Calculated", Toast.LENGTH_SHORT).show();
+    }
+}
